27f23827834a6a69ab1e5dd671070068935d8434
aravind <m.s.aravind@doodleblue.com>
2023-10-30 17:42:09 +0530

    style changes header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,13 +7,13 @@ const Header = (props) => {
 
     return (
         <div className="headerContainer">
-            <div className="flex justify-between h-32 m-16 rounded-full bg-white">
-                <div className='flex justify-start grow p-5 pl-16'>
+            <div className="flex justify-between h-22 m-16 rounded-full bg-white">
+                <div className='flex justify-start grow p-5 pl-14'>
                     <img className="h-auto" src={Logo} alt="logo" />
                 </div>
-                <div className='flex justify-end grow items-center px-3 pr-16'>
-                    <button className="btn-primary mx-3">Ilost for Business</button>
-                    <button className="btn-secondary mx-3">Login / Register</button>
+                <div className='flex justify-end grow items-center px-3 pr-6'>
+                    <button className="btn-secondary mx-3">Ilost for Business</button>
+                    <button className="btn-primary mx-3">Login / Register</button>
                 </div>
             </div>
         </div>
