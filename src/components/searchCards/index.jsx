89935ceb667e0e74ee6ce156c32cdfeb45ce7d52
aravind <m.s.aravind@doodleblue.com>
2023-11-01 10:43:48 +0530

    Code clean up - Need to ad responsive CSS.

--- src/components/searchCards/index.jsx
+++ src/components/searchCards/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaMap, FaCalendar, FaClock, FaArrowRight} from 'react-icons/fa'
+import { FaMap, FaCalendar, FaClock, FaArrowRight } from 'react-icons/fa'
 
 export default function SearchCards(props) {
     return (
@@ -10,17 +10,25 @@ export default function SearchCards(props) {
             </div>
             <div className='absolute bg-white w-[280px] h-[214px] top-[165px] rounded-[18px] flex justify-center items-center'>
                 <div className='w-[240px] h-[180px] mt-[20px]' key={props.idx}>
-                    <div className='font-bold text-2xl'>{props.itemname}</div>
+                    <div className='font-bold text-2xl'>{props.itemName}</div>
                     <div className='h-[66px] w-[133px] mt-[12px]'>
-                        <div className=' w-[150px] flex'><FaMap style={{color: "#00b8b8", margin: '3px'}} />  <h1 className='ml-[5px] font-medium text-sm' >{props.location}</h1></div>
-                        <div className=' w-[150px] flex'><FaCalendar style={{color: "#00b8b8", margin: '3px'}} /> <h1 className='ml-[5px] font-medium text-sm' >{props.date}</h1></div>
-                        <div className='text-sm w-[150px] flex'><FaClock style={{color: "#00b8b8", margin: '3px'}} /> <h1 className='ml-[5px] font-medium text-sm' >{props.time}</h1></div>
+                        <div className=' w-[150px] flex'><FaMap style={{ color: "#00b8b8", margin: '3px' }} />
+                            <h1 className='ml-[5px] font-medium text-sm' >{props.location}</h1>
+                        </div>
+                        <div className=' w-[150px] flex'><FaCalendar style={{ color: "#00b8b8", margin: '3px' }} />
+                            <h1 className='ml-[5px] font-medium text-sm' >{props.date}</h1>
+                        </div>
+                        <div className='text-sm w-[150px] flex'><FaClock style={{ color: "#00b8b8", margin: '3px' }} />
+                            <h1 className='ml-[5px] font-medium text-sm' >{props.time}</h1>
+                        </div>
                     </div>
                     <div className='flex justify-between'>
-                        <div ><button className='h-[42.75px] w-[120px] rounded-[7.5px] bg-[#E8B810] border-none text-sm font-normal mt-[26px]'>Contact</button></div>
+                        <div >
+                            <button className='h-[42.75px] w-[120px] rounded-[7.5px] bg-[#E8B810] border-none text-sm font-normal mt-[26px]'>Contact</button>
+                        </div>
                         <div className='mt-[30px] h-[32px] w-[120px] flex justify-center items-center text-sm font-normal' >
                             <button>View item</button>
-                            <FaArrowRight className=' ml-[5px]'/>
+                            <FaArrowRight className=' ml-[5px]' />
                         </div>
                     </div>
                 </div>
--- src/components/topBase/index.jsx
+++ src/components/topBase/index.jsx
@@ -1,28 +1,28 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
-import addressman from '../../assets/images/AddressMan.png';
+import locateMe from '../../assets/images/location.png';
 
 export default function TopBase() {
-    const[buttonactive,setButtonactive]=useState(true);
-    const[searchkey,setSearchkey] = useState('');
+    const [buttonActive, setButtonActive] = useState(true);
+    const [searchKey, setSearchKey] = useState('');
     const navigate = useNavigate();
 
-    
-    const handlesearchbutton =()=>{
-        setButtonactive(!buttonactive);
+
+    const handleSearchButton = () => {
+        setButtonActive(!buttonActive);
     };
-    
-    const handlereportbutton=()=>{
-        setButtonactive(!buttonactive);
+
+    const handleReportButton = () => {
+        setButtonActive(!buttonActive);
     }
 
-    const handlesearchkey = () =>{
-        localStorage.setItem("searchkey",searchkey);
+    const handleSearchKey = () => {
+        localStorage.setItem("searchKey", searchKey);
         navigate('/findMissingItem');
     }
-  return (
-    <div className='flex '>
-        {/* <div>
+    return (
+        <div className='flex '>
+            {/* <div>
                     {
                         {loginbutton} ? 
                         <Dialog open={loginbutton} style={{ marginLeft: '48%', marginTop:'5%', backgroundColor: 'transparent'}} PaperProps={{style:{height:'500px', width:'540px',display:'flex', alignItems:'center', justifyContent:'center'}}} className='logindialogboxactive'>
@@ -32,27 +32,33 @@ export default function TopBase() {
                         null
                     }
         </div> */}
-        
-        <div className='ml-[70px] mt-[80px]'>
-                    <div className='flex items-center p-[7px] bg-white border-solid rounded-[20px] h-[75px] w-[440px] border border-neutral-200 border-solid'>
-                        <button onClick={handlesearchbutton} className='h-[59px] w-[220px] rounded-[16px] text-[24px] font-normal' style={{backgroundColor: buttonactive? '#266867':'white',color:buttonactive? 'white':'black'}}>Search lost item</button>
-                        <button onClick={handlereportbutton} className='h-[59px] w-[220px] rounded-[16px] text-[24px] font-normal' style={{backgroundColor: buttonactive? 'white':'#266867',color:buttonactive? 'black':'white'}}>Report found item</button>
-                    </div>
-                    {  
-                        buttonactive ? 
+
+            <div className='ml-[70px] mt-[80px]'>
+                <div className='flex items-center p-[7px] bg-white border-solid rounded-[20px] h-[75px] w-[440px] border border-neutral-200'>
+                    <button onClick={handleSearchButton} className='h-[59px] w-[220px] rounded-[16px] text-[24px] font-normal' style={{ backgroundColor: buttonActive ? '#266867' : 'white', color: buttonActive ? 'white' : 'black' }}>
+                        Search lost item
+                    </button>
+                    <button onClick={handleReportButton} className='h-[59px] w-[220px] rounded-[16px] text-[24px] font-normal' style={{ backgroundColor: buttonActive ? 'white' : '#266867', color: buttonActive ? 'black' : 'white' }}>
+                        Report found item
+                    </button>
+                </div>
+                {
+                    buttonActive ?
                         <div className='flex items-center mt-[25px] h-[82px] w-[760px] rounded-[24px] bg-white border border-neutral-200 border-solid'>
-                            <input className=' placeholder:text-black placeholder:text-xl placeholder:pl-[20px] w-[494px] h-[60px] rounded-[16px] ml-[10px] border border-solid border-neutral-200' type='text' placeholder='Search...' value={searchkey} onChange={(e)=>setSearchkey(e.target.value)}/>
-                            <button className='w-[230px] h-[60px] rounded-[16px] border border-solid border-neutral-200 ml-[10px] mr-[10px] text-2xl font-semibold text-white bg-[#E8B810]' onClick={handlesearchkey}>Search</button>
+                            <input className=' placeholder:text-black placeholder:text-xl placeholder:pl-[20px] w-[494px] h-[60px] rounded-[16px] ml-[10px] border border-solid border-neutral-200' type='text' placeholder='Search...' value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
+                            <button className='w-[230px] h-[60px] rounded-[16px] border border-solid border-neutral-200 ml-[10px] mr-[10px] text-2xl font-semibold text-white bg-[#E8B810]' onClick={handleSearchKey}>
+                                Search
+                            </button>
                         </div>
                         :
                         <div className='flex items-center mt-[25px] h-[82px] w-[760px] rounded-[24px] bg-white border border-neutral-200 border-solid'>
-                            <input className='placeholder:text-black placeholder:text-xl placeholder:pl-[20px] w-[494px] h-[60px] rounded-[16px] ml-[10px] border border-solid border-neutral-200' type='text' placeholder='Report...'/>
+                            <input className='placeholder:text-black placeholder:text-xl placeholder:pl-[20px] w-[494px] h-[60px] rounded-[16px] ml-[10px] border border-solid border-neutral-200' type='text' placeholder='Report...' />
                             <button className='searchbox'>Report</button>
                         </div>
-                    
-                    }
-                </div>
-                <div className=' mt-0 ml-[85px] mt-[0px]'><img className='' src={addressman} alt='addressman' height='450px'></img></div>
-    </div>
-  )
+
+                }
+            </div>
+            <div className=' mt-0 ml-[85px]'><img className='' src={locateMe} alt='locateMe' height='450px'></img></div>
+        </div>
+    )
 }
