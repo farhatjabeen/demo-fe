070d8373c16b7f6d780806ccd40d01ffc79571ff
keerthana-K28 <keerthanak28.doodleblue@gmail.com>
2023-11-08 01:02:25 +0530

    updated carousel in business that use platform

--- src/components/ourBrands/index.jsx
+++ src/components/ourBrands/index.jsx
@@ -1,34 +1,56 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import starbucks from '../../assets/images/startbucks.png';
 import amazon from '../../assets/images/amazon.png';
 import airarabia from '../../assets/images/airarabia.png';
 import suitcase from '../../assets/images/suitcase.png';
-import './index.scss';
 
-export default function OurBrands() {
-    const businessbrands = [starbucks, amazon, airarabia, suitcase,starbucks, amazon, airarabia, suitcase];
-    return (
-        <div className='h-80 bg-[#F5F5F5] overflow-hidden'>
-            <div className='font-bold text-4xl text-[#676767] opacity-90 ml-20 pt-9 mb-7'>Businesses that use our platform</div>
-            <div className='flex flex-row w-[1519px] pl-20 whitespace-nowrap animate-marquee'>
-            
-                {businessbrands.map((items,i) => {
-                    return (
-                            <div key={i} className=' flex bg-white mr-5 flex shrink-0 justify-center items-center rounded-3xl w-48 h-44'>
-                                <img src={items} alt='brandimages' />
-                            </div>
-                    );
-                })}
-                {businessbrands.map((items,i) => {
-                    return (
-                            <div key={i} className='flex bg-white mr-5 flex shrink-0 justify-center items-center rounded-3xl w-48 h-44'>
-                                <img src={items} alt='brandimages' />
-                            </div>
-                    );
-                })}
-                
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 5000,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 0,
+  cssEase: 'linear',
+  arrows: false,
+  variableWidth: true,
+};
 
-            </div>
-        </div>
-    )
-}
+const ImageSlide = ({ imageSrc, altText,margin }) => (
+  <div className="w-48 h-44 bg-white rounded-xl shadow-md p-2 m-2">
+    <img src={imageSrc} alt={altText} className={`w-auto h-auto  ${margin}`} />
+  </div>
+);
+
+const OurBrands = () => {
+  const images = [
+    { src: starbucks, alt: 'Starbucks',margin:'my-4 mx-6' },
+    { src: amazon, alt: 'Amazon' ,margin:'my-16 mx-6'},
+    { src: airarabia, alt: 'Air Arabia', margin:'my-12 mx-6'},
+    { src: suitcase, alt: 'Suitcase', margin:'my-4 mx-12'},
+  ];
+
+  const generateImageSlides = () => (
+    images.map((image, index) => (
+      <ImageSlide key={index} imageSrc={image.src} altText={image.alt} margin={image.margin}  />
+    ))
+  );
+
+  return (
+    <div className="w-full overflow-hidden py-6">
+      <div className='font-bold text-4xl text-grey py-6'>Businesses that use our platform</div>
+      <div className="max-w-screen-xl ">
+        <Slider {...settings}>
+          {generateImageSlides()}
+          {generateImageSlides()} 
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default OurBrands;
