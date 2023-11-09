38e2763e8ab143f0eb64980540e09c9d6bfe0a50
Sriyazhini <sriyazhini.doodleblue@gmail.com>
2023-11-09 16:04:51 +0530

    updated header, search report , search cards component and also add more details and view item pages

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Logo from "../../assets/images/logo.png"
-
 import UserLogin from "../userLogin";
+
 
 const Header = (props) => {
     const [loginButton,setLoginButton]=useState(false);
@@ -14,13 +14,13 @@ const Header = (props) => {
     }
     return (
         <div className="headerContainer">
-            <div className="flex justify-between h-22 m-16 rounded-full bg-white">
-                <div className='flex justify-start grow p-5 pl-14'>
-                    <img className="h-auto" src={Logo} alt="logo" />
+            <div className="flex justify-between items-center h-22 m-16 rounded-full bg-white">
+                <div className='flex justify-start grow p-5 xl:pl-12 md:pl-10 sm:pl-8'>
+                    <img className="xl:h-14 xl:w-32 md:h-12 md:w-28 sm:h-10 sm:w-20" src={Logo} alt="logo" />
                 </div>
                 <div className='flex justify-end grow items-center px-3 pr-6'>
-                    <button className="w-64 h-14 border rounded-full border-primary-color text-primary-color text-2xl font-bold mx-3">Ilost for Business</button>
-                    <button className="h-14 w-64 bg-primary-color text-white rounded-full font-bold text-2xl mx-3" onClick={handleLoginButton}>Login / Register</button>
+                    <button className="xl:w-64 xl:h-14 xl:text-2xl md:w-52 md:h-14 md:text-lg sm:w-36 sm:h-12 sm:text-sm font-bold border rounded-full border-primary-color text-primary-color  mx-3">Ilost for Business</button>
+                    <button className="xl:w-64 xl:h-14 xl:text-2xl md:w-52 md:h-14 md:text-lg sm:w-36 sm:h-12 sm:text-sm font-bold bg-primary-color text-white rounded-full xl:mx-1" onClick={handleLoginButton}>Login / Register</button>
                 </div>
                 <div>
                     <UserLogin isOpen={loginButton} onClose={handleLogoutButton} />
--- src/components/searchReport/index.jsx
+++ src/components/searchReport/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {  useState } from 'react'
 import { useNavigate } from 'react-router';
 import addressMan from '../../assets/images/location.png';
 
@@ -21,6 +21,7 @@ export default function SearchReport() {
         setButtonActive(!buttonActive);
     }
 
+    
     const handleSearchKey = () => {
         if (searchKey && locationKey) {
             navigate(`/findMissingItem/${searchKey}/${locationKey}`);
@@ -46,30 +47,30 @@ export default function SearchReport() {
     }
 
     return (
-        <div className='flex xl:flex-row md:flex-col sm:flex-col md:items-center sm:items-center'>
+        <div className='flex xl:pl-3 xl:justify-center xl:flex-row md:flex-col sm:flex-col md:items-center sm:items-center'>
 
-            <div className='xl:mt-20 flex md:flex-col sm:flex-col xl:items-start items-center'>
-                <div className='flex p-2 bg-white border-solid xl:rounded-3xl xl:h-1/5 xl:w-96 md:rounded-2xl md:w-80 sm:h-14 sm:rounded-2xl sm:w-72 border border-[#B6B6B6]'>
-                    <button onClick={handleSearchButton} className='xl:h-14 xl:w-56 xl:rounded-2xl md:w-52 md:h-10 md:rounded-xl md:text-md xl:text-xl sm:rounded-xl sm:text-sm sm:h-10 sm:w-48 font-normal' style={{ backgroundColor: buttonActive ? '#266867' : 'white', color: buttonActive ? 'white' : 'black' }}>Search lost item</button>
-                    <button onClick={handleReportButton} className='xl:h-14 xl:w-56 xl:rounded-2xl md:w-52 md:h-10 md:rounded-xl md:text-md xl:text-xl sm:rounded-xl sm:text-sm sm:h-10 sm:w-48 font-normal' style={{ backgroundColor: buttonActive ? 'white' : '#266867', color: buttonActive ? 'black' : 'white' }}>Report found item</button>
+            <div className='xl:mt-20 xl:mb-20 flex xl:items-start md:flex-col sm:flex-col items-center'>
+                <div className='flex sm:items-center p-2 bg-white border-solid xl:rounded-3xl xl:h-1/5 xl:w-96 md:rounded-2xl md:h-14 md:w-80 sm:h-12 sm:rounded-2xl sm:w-64 border border-[#B6B6B6]'>
+                    <button onClick={handleSearchButton} className='xl:h-14 xl:w-56 xl:rounded-2xl md:w-52 md:h-10 md:rounded-xl md:text-md xl:text-xl sm:rounded-xl sm:text-sm sm:h-9 sm:w-44 font-normal' style={{ backgroundColor: buttonActive ? '#266867' : 'white', color: buttonActive ? 'white' : 'black' }}>Search lost item</button>
+                    <button onClick={handleReportButton} className='xl:h-14 xl:w-56 xl:rounded-2xl md:w-52 md:h-10 md:rounded-xl md:text-md xl:text-xl sm:rounded-xl sm:text-sm sm:h-9 sm:w-44 font-normal' style={{ backgroundColor: buttonActive ? 'white' : '#266867', color: buttonActive ? 'black' : 'white' }}>Report found item</button>
                 </div>
                 {
                     buttonActive ?
-                        <div className='flex items-center mt-6 xl:h-20 xl:w-2xl xl:rounded-3xl md:h-16 md:w-11/12 md:rounded-2xl sm:w-full sm:h-16 sm:rounded-2xl bg-white border border-[#B6B6B6] border-solid'>
-                            <input className={`placeholder:text-black placeholder:text-base xl:w-60 xl:h-16 p-4 xl:rounded-2xl md:h-12 md:w-44 md:rounded-xl sm:rounded-xl sm:w-44 sm:h-12 ml-2.5 border border-solid ${searchValue ? 'border-[#B6B6B6]' : 'border-[#FF0000]'}`} type='text' placeholder='Search...' value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
-                            <input className={`placeholder:text-black placeholder:text-base xl:w-60 xl:h-16 p-4 xl:rounded-2xl md:h-12 md:w-44 md:rounded-xl sm:rounded-xl sm:w-44 sm:h-12 ml-2.5 border border-solid ${location ? 'border-[#B6B6B6]' : 'border-[#FF0000]'}`} type='text' placeholder='Location' value={locationKey} onChange={(e) => setLocationKey(e.target.value)} />
-                            <button className='xl:w-52 xl:h-16 xl:rounded-2xl xl:text-2xl md:w-44 md:h-12 md:rounded-xl md:text-lg sm:h-12 sm:w-36 sm:rounded-xl font-semibold text-white bg-primary-color border border-solid border-[#B6B6B6] mx-2.5' onClick={handleSearchKey}>Search</button>
+                        <div className='flex items-center mt-6 xl:h-20 xl:w-xl xl:rounded-3xl md:h-16 md:w-xl md:rounded-2xl sm:w-xl sm:h-14 sm:rounded-2xl bg-white border border-[#B6B6B6] border-solid'>
+                            <input className={`placeholder:text-black placeholder:text-base xl:w-60 xl:h-16 p-4 xl:rounded-2xl md:h-12 md:w-52 md:rounded-xl sm:rounded-xl sm:w-40 sm:h-10 ml-2.5 border border-solid ${searchValue ? 'border-[#B6B6B6]' : 'border-[#FF0000]'}`} type='text' placeholder='Search...' value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
+                            <input className={`placeholder:text-black placeholder:text-base xl:w-60 xl:h-16 p-4 xl:rounded-2xl md:h-12 md:w-52 md:rounded-xl sm:rounded-xl sm:w-40 sm:h-10 ml-2.5 border border-solid ${location ? 'border-[#B6B6B6]' : 'border-[#FF0000]'}`} type='text' placeholder='Location' value={locationKey} onChange={(e) => setLocationKey(e.target.value)} />
+                            <button className='xl:w-52 xl:h-16 xl:rounded-2xl xl:text-2xl md:w-38 md:h-12 md:rounded-xl md:text-lg sm:h-10 sm:w-32 sm:rounded-xl font-semibold text-white bg-primary-color border border-solid border-[#B6B6B6] mx-2.5' onClick={handleSearchKey}>Search</button>
                         </div>
                         :
-                        <div className='flex items-center mt-6 xl:h-20 xl:w-2xl xl:rounded-3xl md:h-16 md:w-11/12 md:rounded-2xl sm:w-full sm:h-16 sm:rounded-2xl bg-white border border-[#B6B6B6] border-solid'>
-                            <input className={`placeholder:text-black placeholder:text-base p-4 xl:w-60 xl:h-16 xl:rounded-2xl md:h-12 md:w-44 md:rounded-xl sm:rounded-xl sm:w-44 sm:h-12 ml-2.5 border border-solid border-[#B6B6B6] ${reportValue ? 'border-[#B6B6B6]' : 'border-[#FF0000]'}`} type='text' placeholder='Describe Item' value={reportKey} onChange={(e) => setReportKey(e.target.value)} />
-                            <input className={`placeholder:text-black placeholder:text-base p-4 xl:w-60 xl:h-16 xl:rounded-2xl md:h-12 md:w-44 md:rounded-xl sm:rounded-xl sm:w-44 sm:h-12 ml-2.5 border border-solid border-[#B6B6B6] ${location ? 'border-[#B6B6B6]' : 'border-[#FF0000]'}`} type='text' placeholder='Location' value={locationKey} onChange={(e) => setLocationKey(e.target.value)} />
-                            <button className='xl:w-52 xl:h-16 xl:rounded-2xl xl:text-2xl md:w-44 md:h-12 md:rounded-xl md:text-lg sm:h-12 sm:w-36 sm:rounded-xl border border-solid border-[#B6B6B6] mx-2.5 font-semibold text-white bg-primary-color' onClick={handleReportKey}>Continue</button>
+                        <div className='flex items-center mt-6 xl:h-20 xl:w-2xl xl:rounded-3xl md:h-16 md:w-xl md:rounded-2xl sm:w-xl sm:h-14 sm:rounded-2xl bg-white border border-[#B6B6B6] border-solid'>
+                            <input className={`placeholder:text-black placeholder:text-base p-4 xl:w-60 xl:h-16 xl:rounded-2xl md:h-12 md:w-52 md:rounded-xl sm:rounded-xl sm:w-40 sm:h-10 ml-2.5 border border-solid border-[#B6B6B6] ${reportValue ? 'border-[#B6B6B6]' : 'border-[#FF0000]'}`} type='text' placeholder='Describe Item' value={reportKey} onChange={(e) => setReportKey(e.target.value)} />
+                            <input className={`placeholder:text-black placeholder:text-base p-4 xl:w-60 xl:h-16 xl:rounded-2xl md:h-12 md:w-52 md:rounded-xl sm:rounded-xl sm:w-40 sm:h-10 ml-2.5 border border-solid border-[#B6B6B6] ${location ? 'border-[#B6B6B6]' : 'border-[#FF0000]'}`} type='text' placeholder='Location' value={locationKey} onChange={(e) => setLocationKey(e.target.value)} />
+                            <button className='xl:w-52 xl:h-16 xl:rounded-2xl xl:text-2xl md:w-38 md:h-12 md:rounded-xl md:text-lg sm:h-10 sm:w-32 sm:rounded-xl border border-solid border-[#B6B6B6] mx-2.5 font-semibold text-white bg-primary-color' onClick={handleReportKey}>Continue</button>
                         </div>
 
                 }
             </div>
-            <div className='xl:ml-24'><img className='xl:h-11/12 md:h-11/12 md:w-11/12' src={addressMan} alt='addressMan' ></img></div>
+            <div className='xl:ml-24'><img className='xl:h-11/12 md:h-11/12 md:w-11/12 sm:h-10/12 sm:w-10/12' src={addressMan} alt='addressMan' ></img></div>
         </div>
     )
 }
