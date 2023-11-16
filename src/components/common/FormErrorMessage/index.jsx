e22be15e1342400492750f9f23e496ccb0073a76
aravind <m.s.aravind@doodleblue.com>
2023-11-16 17:52:20 +0530

    Form validation and API integration

---
+++ src/components/common/FormErrorMessage/index.jsx
@@ -0,0 +1,25 @@
+/**
+ * FormErrorMessage: Common Re-usable Component to show the error messages.
+ * @return {JSX.Element} The JSX Code for FormErrorMessage
+ */
+import PropTypes from 'prop-types';
+
+export const FormErrorMessage = ({ error }) => {
+  if (!error) return null;
+
+  return (
+    <span className="text-danger fs-14 fw-500">
+      {error.message ? error.message : ''}
+    </span>
+  );
+};
+
+FormErrorMessage.propTypes = {
+  error: PropTypes.oneOf([
+    { message: PropTypes.string },
+    null
+  ])
+};
+FormErrorMessage.defaultProps = {
+  error: [{ message: '' }]
+};
--- src/components/common/button/index.jsx
+++ src/components/common/button/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-export default function Button({ props }) {
+export default function Button({
+    width,
+    height,
+    bgColor,
+    textColor = 'black',
+    textSize = 'sm',
+    isBorder,
+    borderColor = 'black',
+    roundedValue,
+    fontWeight,
+    mxValue,
+    buttonTitle = 'save'
+}) {
     return (
         <div>
-            <button className={`w-${props.width} h-${props.height} bg-${props.bgColor} text-${props.textColor} text-${props.textSize} 
-            ${props.isBorder ? 'border' : ''} border-${props.borderColor} rounded-${props.roundedValue} font-${props.fontWeight} mx-${props.mxValue}`}>
-                {props.buttonTitle}
+            <button className={`w-${width} h-${height} bg-${bgColor} text-${textColor} text-${textSize} 
+            ${isBorder ? 'border' : ''} border-${borderColor} rounded-${roundedValue} font-${fontWeight} mx-${mxValue}`}>
+                {buttonTitle}
             </button>
         </div>
     )
