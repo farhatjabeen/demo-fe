2e0158e7714eaa223d722ba87461d8c8acc81b81
keerthana-K28 <keerthanak28.doodleblue@gmail.com>
2023-11-23 20:23:47 +0530

    api integration

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import itemsSlice from "./itemsSlice";
+import adminSlice from "./adminSlice";
 
 const reducers = combineReducers({
     user: userSlice,
-    items: itemsSlice
+    items: itemsSlice,
+    foundItems: adminSlice,
 });
 
 export default reducers;
