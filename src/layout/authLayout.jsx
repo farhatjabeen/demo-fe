22a6efe055b3b304310d1c48bc534310e14274a7
aravind <m.s.aravind@doodleblue.com>
2023-11-24 07:28:02 +0530

    commented un used codes

--- src/layout/authLayout.jsx
+++ src/layout/authLayout.jsx
@@ -4,23 +4,23 @@ import Footer from "../components/footer";
 
 export function AuthLayout(props) {
 
-  const [headerContent, setHtmlContent] = useState('');
-  const [footerContent, setFooterContent] = useState('');
+  // const [headerContent, setHtmlContent] = useState('');
+  // const [footerContent, setFooterContent] = useState('');
 
-  useEffect(() => {
-    async function fetchHeaderContent() {
-      const response = await fetch(process.env.REACT_APP_HEADER_URL);
-      const html = await response.text();
-      setHtmlContent(html);
-    }
-    async function fetchFooterContent() {
-      const res = await fetch(process.env.REACT_APP_FOOTER_URL);
-      const footer = await res.text();
-      setFooterContent(footer);
-    }
-    fetchHeaderContent();
-    fetchFooterContent();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchHeaderContent() {
+  //     const response = await fetch(process.env.REACT_APP_HEADER_URL);
+  //     const html = await response.text();
+  //     setHtmlContent(html);
+  //   }
+  //   async function fetchFooterContent() {
+  //     const res = await fetch(process.env.REACT_APP_FOOTER_URL);
+  //     const footer = await res.text();
+  //     setFooterContent(footer);
+  //   }
+  //   fetchHeaderContent();
+  //   fetchFooterContent();
+  // }, []);
 
 
   return (
