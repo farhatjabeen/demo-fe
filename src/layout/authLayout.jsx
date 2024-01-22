af87c9ea269fc2c75ae97557675fb97f574b987c
aravind <m.s.aravind@doodleblue.com>
2024-01-22 11:11:29 +0530

    Background Image handling css

--- src/layout/authLayout.jsx
+++ src/layout/authLayout.jsx
@@ -1,30 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import { useLocation } from 'react-router-dom'
 
 export function AuthLayout(props) {
+  const [customClassName, setCustomClassName] = useState("default")
+  const location = useLocation();
 
-  // const [headerContent, setHtmlContent] = useState('');
-  // const [footerContent, setFooterContent] = useState('');
-
-  // useEffect(() => {
-  //   async function fetchHeaderContent() {
-  //     const response = await fetch(process.env.REACT_APP_HEADER_URL);
-  //     const html = await response.text();
-  //     setHtmlContent(html);
-  //   }
-  //   async function fetchFooterContent() {
-  //     const res = await fetch(process.env.REACT_APP_FOOTER_URL);
-  //     const footer = await res.text();
-  //     setFooterContent(footer);
-  //   }
-  //   fetchHeaderContent();
-  //   fetchFooterContent();
-  // }, []);
+  useEffect(() => {
+    console.log("path Name", location.pathname)
+    if (location.pathname.split("/").includes("findMissingItem") || location.pathname.split("/").includes("addMoreDetails")) {
+      setCustomClassName("missing-screen")
+    } else if (location.pathname.split("/").includes("mylistings")) {
+      setCustomClassName("my-listing")
+    }
+  }, [location, customClassName])
 
 
   return (
-    <div className="authLayout flex flex-col min-h-screen">
+    <div className={`authLayout-${customClassName} layout-image flex flex-col min-h-screen`}>
       <Header />
       <div className="flex-grow">
         <main id="authLayoutContainer">{props?.children}</main>
